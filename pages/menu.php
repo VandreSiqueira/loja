dd2a1d04b2ff3a82517de6d53ee9c92ca8bf86c6
Vandré Siqueira <vandreifpe@gmail.com>
2022-06-14 12:20:34 -0300

    correcoes de CSS

--- pages/menu.php
+++ pages/menu.php
@@ -8,17 +8,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="/styles/menu.css" />
-    <link rel="stylesheet" type="text/css" href="/styles/style.css" />
+    <link rel="stylesheet" type="text/css" href="../styles/menu.css" />
+    <link rel="stylesheet" type="text/css" href="../styles/style.css" />
     <title>Menu - Loja</title>
 </head>
 <body>
     <div class="content">
         <div id="opcoes">
             <h2>Menu</h2>
-            <a id="op1" href="/pages/cadastrarCliente.html">Cadastrar Cliente</a>
-            <a id="op2" href="/pages/fazerPedido.html">Fazer pedido</a>
-            <a id="op3" href="/pages/verEstoque.html">Ver estoque</a>
+            <a id="op1" href="cadastrarCliente.html">Cadastrar Cliente</a>
+            <a id="op2" href="fazerPedido.html">Fazer pedido</a>
+            <a id="op3" href="verEstoque.html">Ver estoque</a>
             <a id="op4" href="sair.php">Sair</a>
         </div>
     </div>
