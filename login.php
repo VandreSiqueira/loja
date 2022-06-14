8d02f14887709ad1beb07cf44275a186c94371fb
Vandré Siqueira <vandreifpe@gmail.com>
2022-06-13 23:17:20 -0300

    alterações

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ include('conexao.php');
 session_start();
 if(empty($_POST['email']) || empty($_POST['senha'])){
     header('location: index.html');
-    exit();
+    exit;
 }
 $email = mysqli_real_escape_string($con, empty($_POST['email']));
 $senha = mysqli_real_escape_string($con, empty($_POST['senha']));
@@ -11,13 +11,13 @@ $senha = mysqli_real_escape_string($con, empty($_POST['senha']));
 $query = "SELECT email, senha FROM vendedor WHERE email = {$email} and senha = {$senha}";
 
 $result = mysqli_query($con, $query);
-
-$row = mysqli_num_rows($result);
-
-if($rows == 1){
-    $_SESSION['vendedor'] = $email
-    exit();
+echo $result;
+/*
+if($row == 1){
+    $_SESSION['vendedor'] = $email;
+    exit;
 } else {
     header('location: index.html');
-    exit();
+    exit;
 }
+*/
