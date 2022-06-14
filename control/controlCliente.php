cecc05375eb8d09220110ceb931d0112db0082e5
pedrovalcantara16 <pedro-vinicius20@hotmail.com>
2022-06-14 15:21:38 -0300

    CADASTRANDO CLIENTE

--- control/controlCliente.php
+++ control/controlCliente.php
@@ -1,29 +1,27 @@
 <?php
-// include database connection file
-require_once'conexao.php';
-if(isset($_POST['cadastrar']))
-{
-// Posted Values  
-$nome=$_POST['nome'];
-$sobrenome=$_POST['sobrenome'];
-$cpf=$_POST['cpf'];
-$email=$_POST['email'];
-$endereco = $_POST['endereco'];
-$endereco_id = mysqli_query($con,"SELECT E.id FROM endereco AS E WHERE E.endereco LIKE '%$endereco%'");
-$data_criacao = date('d/m/Y H:i');
 
-// Call the store procedure for insertion
-$sql=mysqli_query($con,"call inserir_cliente('$nome','$sobrenome','$cpf','$email','$endereco_id', '$data_criacao')");
+include('../conexao.php');
+if(isset($_POST['cadastrar'])){
+   
+    $nome=$_POST['nome'];
+    $sobrenome=$_POST['sobrenome'];
+    $cpf=$_POST['cpf'];
+    $email=$_POST['email'];
 
-if($sql){
-// Message for successfull insertion
-echo "<script>alert('Cadastrado com sucesso!');</script>";
-echo "<script>window.location.href='menu.php'</script>"; 
-}
-else {
-// Message for unsuccessfull insertion
-echo "<script>alert('Houve algum erro! Tente novamente');</script>";
-echo "<script>window.location.href='cadastrarCliente.php'</script>"; 
-}
+    $endereco = $_POST['endereco'];
+    $query_endereco = mysqli_query($con, "SELECT E.id FROM endereco AS E WHERE E.endereco LIKE '%$endereco%'");
+    $endereco_id = $query_endereco->fetch_array()[0];
+
+
+    $exec=mysqli_query($con,"CALL inserir_cliente('$nome','$sobrenome','$cpf','$email','$endereco_id')");
+
+    if($exec){
+        echo "<script>alert('Cadastrado com sucesso!');</script>";
+        echo "<script>window.location.href='../pages/menu.php'</script>"; 
+    }
+    else {
+        echo "<script>alert('Houve algum erro! Tente novamente');</script>";
+        echo "<script>window.location.href='../pages/menu.php'</script>"; 
+    }
 }
 ?>
