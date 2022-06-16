c72c8da29ea8dc79e528a7142b0d71f18f01aa41
Vandré Siqueira <vandreifpe@gmail.com>
2022-06-15 23:22:01 -0300

    cadastrarCidade

--- pages/cadastrarCidade.php
+++ pages/cadastrarCidade.php
@@ -20,7 +20,19 @@ include('../conexao.php');
             <input type="text" name="cidade" />
             <label>Pais</label>
             <select name="pais">
-                <option>Selecione um país...</option>
+            <?php 
+                    $sql =mysqli_query($con, "SELECT P.pais FROM pais as P");
+                    $cnt=1;
+                    $row=mysqli_num_rows($sql);
+                    if($row>0){
+                    while ($result=mysqli_fetch_array($sql)) {           
+                    ?>  
+                        <option><?php echo htmlentities($result['pais']);?></option>                      
+                    <?php 
+        
+                    $cnt++;
+                    } }
+            ?>
             </select>
             <input id="submit" type="submit" name="cadastrar" value="Cadastrar" />
         </form>
